f0ca94d8b0df752e69361e4c13d1199e947c9c10
shlee3 <shlee3@nkia.co.kr>
2024-08-26 15:01:27 +0900

    feat: MembershipPersistentAdapter 구

---
+++ membership-service/src/main/kotlin/membership/adapter/out/persistent/MembershipPersistentAdapter.kt
@@ -0,0 +1,27 @@
+package membership.adapter.out.persistent
+
+import common.PersistentAdapter
+import membership.application.port.out.RegisterMembershipPort
+import membership.domain.Membership
+
+@PersistentAdapter
+class MembershipPersistentAdapter(
+    private val membershipRepository: MembershipRepository
+) : RegisterMembershipPort {
+    override fun createMembership(
+        membershipName: Membership.MembershipName,
+        membershipEmail: Membership.MembershipEmail,
+        membershipAddress: Membership.MembershipAddress,
+        membershipIsCorp: Membership.MembershipIsCorp
+    ): MembershipEntity {
+        val entity = membershipRepository.save(
+            MembershipEntity.new(
+                name = membershipName.value,
+                email = membershipEmail.value,
+                address = membershipAddress.value,
+                isCorp = membershipIsCorp.value
+            )
+        )
+        return entity
+    }
+}
